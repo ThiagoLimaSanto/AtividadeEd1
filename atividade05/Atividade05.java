c365429a6a97718be2db76b9e2b8514da80150b6
Thiago Lima <159193770+ThiagoLimaSanto@users.noreply.github.com>
2024-08-06 10:31:12 -0300

    Add files via upload

---
+++ atividade05/Atividade05.java
@@ -0,0 +1,13 @@
+package com.mycompany.atividade05;
+
+public class Atividade05{
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        
+        
+        lista.remover(0);
+        
+        
+        System.out.println(lista.busca(4));
+    }
+}
---
+++ atividade05/No.java
@@ -0,0 +1,12 @@
+package com.mycompany.atividade05;
+
+class No {
+
+    int info;
+    No prox;
+
+    public No(int info) {
+        this.info = info;
+        this.prox = null;
+    }
+}
